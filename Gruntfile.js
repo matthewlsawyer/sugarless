2e93c4cd9f1aa28a46db745c66858e7db0694dec
Matthew L. Sawyer <matthewlsawyer@gmail.com>
2014-08-19 16:57:42 -0400

    whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,22 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		less : {
-			development : {
-				options: {},
-				files : {
-					"dist/css/sugar.css" : "src/less/sugar.less"
-				}
-			},
-			production : {
-				options: {
-					compress : true,
-					cleancss : true
-				},
-				files : {
-					"dist/css/sugar.min.css" : "src/less/sugar.less"
-				}
-			}
-		},
+  grunt.initConfig({
+    less : {
+      development : {
+        options: {},
+        files : {
+          "dist/css/sugar.css" : "src/less/sugar.less"
+        }
+      },
+      production : {
+        options: {
+          compress : true,
+          cleancss : true
+        },
+        files : {
+          "dist/css/sugar.min.css" : "src/less/sugar.less"
+        }
+      }
+    },
     copy : {
       main : {
         files : [
@@ -24,8 +24,8 @@ module.exports = function(grunt) {
         ]
       }
     }
-	});
-	grunt.loadNpmTasks("grunt-contrib-less");
+  });
+  grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-contrib-copy");
-	grunt.registerTask("default", [ "less", "copy" ]);
+  grunt.registerTask("default", [ "less", "copy" ]);
 };
