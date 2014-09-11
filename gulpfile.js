8ca44929a457b035578d809a6af26cb7fd0b46f4
Matt Sawyer <msawyer@562-osx.local>
2014-09-11 14:52:31 -0400

    changed to use gulp

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var del     = require("del");
+var bower	  = require("bower");
+var gulp    = require("gulp");
+var less    = require("gulp-less");
+var rename  = require("gulp-rename");
+var minify  = require("gulp-minify-css");
+
+var paths = {
+  bowerDir : "bower_components/",
+  lessSrc : "src/less/sugar.less",
+  cssDest : "dist/css/",
+  fontDest : "dist/fonts"
+};
+
+gulp.task("default", ["build"]);
+
+gulp.task("build", ["clean", "less", "fonts"]);
+
+gulp.task("clean", function(cb) {
+  del("dist", cb);
+});
+
+gulp.task("less", ["bower"], function() {
+  gulp.src(paths.lessSrc)
+  .pipe(less())
+  .pipe(rename("sugar.css"))
+  .pipe(gulp.dest(paths.cssDest))
+  .pipe(minify())
+  .pipe(rename("sugar.min.css"))
+  .pipe(gulp.dest(paths.cssDest));
+});
+
+gulp.task("fonts", ["bower"], function() {
+  gulp.src(paths.bowerDir + "font-awesome/fonts/*.*")
+  .pipe(gulp.dest(paths.fontDest));
+});
+
+gulp.task("bower", function(cb) {
+  bower.commands.install()
+  .on("end", function() {
+    cb();
+  });
+});
